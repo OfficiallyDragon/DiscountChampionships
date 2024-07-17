f8fb86ae3d56c53b791774af276116160240c028
OfficiallyDragon <dexter@dexterwheatcroft.uk>
2024-07-17 10:33:08 +0100

    Initial commit

---
+++ src/main/kotlin/me/officiallydragon/discountchampionships/DiscountChampionships.kt
@@ -0,0 +1,14 @@
+package me.officiallydragon.discountchampionships
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DiscountChampionships : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
